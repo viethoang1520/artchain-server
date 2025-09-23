8eea7e7226cd298ce091972245c2978a5dc06ff2
viethoang1520 <viethoang1520.dev@gmail.com>
2025-09-23 22:54:49 +0700

    fix: database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USERNAME', 'postgres'),
+        password: configService.get<string>('DB_PASSWORD', ''),
+        database: configService.get<string>('DB_NAME', 'artchain'),
+        synchronize: false,
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService]
+    }),
+  ],
+  controllers:  [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
