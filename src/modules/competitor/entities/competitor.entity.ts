c083b8feb4590050e17636cdfbe602aead028696
nhathao04 <nhathao06012004.bl@gmail.com>
2025-09-29 12:41:05 +0700

    feat: update user's information

--- src/modules/competitor/entities/competitor.entity.ts
+++ src/modules/competitor/entities/competitor.entity.ts
@@ -1,11 +1,4 @@
-import {
-  Entity,
-  PrimaryColumn,
-  Column,
-  OneToOne,
-  JoinColumn,
-  ManyToOne,
-} from 'typeorm';
+import { Entity, PrimaryColumn, Column, OneToOne, JoinColumn } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 
 @Entity('competitor')
--- src/modules/profile/profile.module.ts
+++ src/modules/profile/profile.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 import { Competitor } from '../competitor/entities/competitor.entity';
 import { Examiner } from '../examiner/entities/examiner.entity';
-// import { Achievement } from '../competitor/entities/achievement.entity';
 import { Contest } from '../contest/entities/contest.entity';
 import { ContestExaminer } from '../contest/entities/contest-examiner.entity';
 import { ProfileController } from './profile.controller';
@@ -15,7 +14,6 @@ import { ProfileService } from './profile.service';
       User,
       Competitor,
       Examiner,
-      // Achievement,
       Contest,
       ContestExaminer,
     ]),
