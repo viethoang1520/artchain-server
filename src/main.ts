1906ecc31e979ad56f690391117b1d48bd5f0c68
viethoang1520 <viethoang1520.dev@gmail.com>
2025-10-01 10:47:39 +0700

    feat: swagger bearer auth

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     .setDescription('The Artchain API')
     .setVersion('1.0')
     .addTag('artchain')
+    .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
