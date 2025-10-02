74aa497d406aa0fc7b08c18a4fba5dd8889e4565
nhathao04 <nhathao06012004.bl@gmail.com>
2025-10-02 19:58:56 +0700

    fix: add auth module

--- src/modules/contests/contests.module.ts
+++ src/modules/contests/contests.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContestsService } from './contests.service';
 import { ContestsController } from './contests.controller';
 import { Contest } from './entities/contests.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Contest])],
+  imports: [
+    TypeOrmModule.forFeature([Contest]),
+    AuthModule
+  ],
   controllers: [ContestsController],
   providers: [ContestsService],
   exports: [ContestsService],
