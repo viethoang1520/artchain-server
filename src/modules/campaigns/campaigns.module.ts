d584c17b03e6dfa559d9085166741ad812407a56
nhathao04 <nhathao06012004.bl@gmail.com>
2025-11-15 02:39:37 +0700

    fix: return current amount of campaign

--- src/modules/campaigns/campaigns.module.ts
+++ src/modules/campaigns/campaigns.module.ts
@@ -4,9 +4,10 @@ import { CampaignsController } from './campaigns.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Campaign } from './entities/campaign.entity';
 import { Sponsor } from '../sponsors/entities/sponsor.entity';
+import { Transaction } from '../payments/entities/transaction.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Campaign, Sponsor])],
+  imports: [TypeOrmModule.forFeature([Campaign, Sponsor, Transaction])],
   controllers: [CampaignsController],
   providers: [CampaignsService],
   exports: [CampaignsService],
