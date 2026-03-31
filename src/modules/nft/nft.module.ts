0eb7dc60ae00a6e990c3aaf9401e029e7898e072
nhathao04 <nhathao06012004.bl@gmail.com>
2026-04-01 01:13:54 +0700

    feat(nft): mint painting

--- src/modules/nft/nft.module.ts
+++ src/modules/nft/nft.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NftService } from './nft.service';
 import { NftController } from './nft.controller';
-import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Nft } from '../paintings/entities/nft.entity';
+import { Painting } from '../paintings/entities/paintings.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Nft, Painting])],
   controllers: [NftController],
   providers: [NftService],
-  imports: [HttpModule],
 })
 export class NftModule {}
---
+++ src/modules/paintings/entities/nft.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('nfts')
+export class Nft {
+  @PrimaryColumn({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    name: 'transaction_hash',
+  })
+  transactionHash: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true, name: 'cid' })
+  cid: string;
+}
