12c8f49654b7b3ec93c6b5cbdf6262e57d226385
nhathao04 <nhathao06012004.bl@gmail.com>
2026-03-31 23:18:27 +0700

    fix(wallet): check import

--- src/modules/wallets/dto/create-wallet.dto.ts
+++ src/modules/wallets/dto/create-wallet.dto.ts
@@ -1,14 +1,12 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty } from "class-validator/types/decorator/common/IsNotEmpty";
-import { IsUUID } from "class-validator/types/decorator/string/IsUUID";
-import { UUID } from "crypto";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsUUID } from 'class-validator';
 
-export class CreateWalletDto{
+export class CreateWalletDto {
   @ApiProperty({
     description: 'ID của user sở hữu ví',
     example: 'ba9b73f4-bdfd-4374-b289-5c1fcff701fe',
   })
   @IsNotEmpty()
-  @IsUUID()   
-  accountId: UUID
+  @IsUUID()
+  accountId: string;
 }
